cb1476f3ee6dc3db2dd0cfb4d37669ea3b7719c1
Christopher <cuadradochris@hotmail.com>
2022-12-14 15:53:19 -0500

    more cards added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,6 +21,38 @@ const Card = () => {
           alt="/"
         />
       </div>
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="/"
+        />
+      </div>
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Just Desserts</p>
+          <p className="px-2">For That Sweet Tooth...</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="/"
+        />
+      </div>
     </div>
   );
 };
